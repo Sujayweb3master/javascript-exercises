6f6dc799d1a8b67cdda24477f414aa44caa27882
Sujayweb3master <122873224+Sujayweb3master@users.noreply.github.com>
2023-10-26 18:02:38 +0530

    Add solution to the second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    const temp = string;
+
+    if (num === 0) {
+        return "";
+    } else if (num < 0) {
+        return "ERROR";
+    }
+
+    for(let i = 1; i < num; i++) {
+        string += temp;
+    }
+
+    return string;
 };
 
 // Do not edit below this line
