2e892eaf0d04a920ed88f8e8e68ea9a9ffaa737f
Sujayweb3master <122873224+Sujayweb3master@users.noreply.github.com>
2023-10-27 12:17:05 +0530

    Add solution to the 4th exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, removeOne, removeTwo, removeThree, removeFour) {
+    console.log(arr);
+    // for (let i = 0; i < arr.length ; i++) {
+        
+    // if (arr[i] === removeOne || arr[i] === removeTwo || arr[i] === removeThree || arr[i] === removeFour)
+    //     arr.splice(i, 1);
+    // }    
+    // console.log(arr);  
+    // return arr;
 
+    let i = 0;
+    
+    while(i < arr.length) {
+        if (arr[i] === removeOne || arr[i] === removeTwo || arr[i] === removeThree || arr[i] === removeFour) {
+            arr.splice(i, 1);
+            i = 0;
+            continue;
+        }
+        i++;
+    }   
+    
+    return arr;
+    
 };
 
 // Do not edit below this line
