d665c41364243e39c96cfb7a8f10ae916d78b4d6
Sujayweb3master <122873224+Sujayweb3master@users.noreply.github.com>
2023-10-27 11:44:57 +0530

    Add solution to the 3rd exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let n = str.length;
+    let reverseStringValue = "";
 
+    for (let i = n-1; i >= 0; i--) {
+        console.log(str[i]);
+        reverseStringValue += str[i];
+        
+    }
+    return reverseStringValue; 
 };
 
 // Do not edit below this line
