2dee455c57b2c8608f4498fa5b5e7a1e5ab15848
Sujayweb3master <122873224+Sujayweb3master@users.noreply.github.com>
2023-10-27 16:03:36 +0530

    Add solution to the exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
+    
+    if (start > end) {
+        start = start + end;
+        end = start - end;
+        start = start - end;
+    } 
 
-};
+    if ((typeof(start) === "string") || (typeof(end) === "string") || (start < 0) || (end < 0) || (typeof(end) === "object")) {
+
+        return "ERROR";
+
+    } else {
+        for (let i = start; i <= end; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    };
 
 // Do not edit below this line
 module.exports = sumAll;
